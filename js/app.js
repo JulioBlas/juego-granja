51c335b0e787df184cd091bd31b238d373ac5cef
JulioBlas <jblaa38@gmail.com>
2018-10-16 15:28:07 -0500

    Dibujando objetos en el tablero

--- js/app.js
+++ js/app.js
@@ -6,14 +6,34 @@ var fondo = {
     imagen: Image
 };
 
+var vaca = {
+    url: './imagenes/vaca.png',
+    imagen: Image
+};
+
+var cerdo = {
+    url: './imagenes/cerdo.png',
+    imagen :Image
+}
+
 fondo.imagen = new Image ();
+vaca.imagen = new Image();
+cerdo.imagen = new Image();
 fondo.imagen.src = fondo.url;
-
+vaca.imagen.src = vaca.url;
+cerdo.imagen.src = cerdo.url;
 fondo.imagen.addEventListener("load", function(){
-    dibujar();
-})
-
+    lapiz.drawImage(fondo.imagen,0,0);
+});
+vaca.imagen.addEventListener("load",function(){
+     lapiz.drawImage(vaca.imagen,150,100);
+});
+cerdo.imagen.addEventListener("load",function(){
+     lapiz.drawImage(cerdo.imagen,200,200);
+});
 function dibujar(){
-    lapiz.drawImage(fondo.image,0,0);
+    lapiz.drawImage(fondo.imagen,0,0);
+    lapiz.drawImage(vaca.imagen,150,100);
+    lapiz.drawImage(cerdo.imagen,200,200);
 }
 
