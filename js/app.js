f8200ed546c2ff7e49a45899b6435d7153f687d3
JulioBlas <jblaa38@gmail.com>
2018-10-17 08:37:51 -0500

    Mostrando todas las imagenes en la pagina

--- js/app.js
+++ js/app.js
@@ -3,37 +3,64 @@ var lapiz = canvas.getContext('2d');
 
 var fondo = {
     url: './imagenes/tile.png',
-    imagen: Image
+    imagen: Image,
+    cargaOk : false
 };
 
 var vaca = {
     url: './imagenes/vaca.png',
-    imagen: Image
+    imagen: Image,
+    cargaOk : false
 };
 
 var cerdo = {
     url: './imagenes/cerdo.png',
-    imagen :Image
-}
+    imagen :Image,
+    cargaOk : false
+};
+
+var cuchillo = {
+    url: './imagenes/cuchillo.png',
+    imagen : Image,
+    cargaOk: false
+};
 
 fondo.imagen = new Image ();
 vaca.imagen = new Image();
 cerdo.imagen = new Image();
+cuchillo.imagen = new Image();
 fondo.imagen.src = fondo.url;
 vaca.imagen.src = vaca.url;
 cerdo.imagen.src = cerdo.url;
+cuchillo.imagen.src = cuchillo.url;
 fondo.imagen.addEventListener("load", function(){
-    lapiz.drawImage(fondo.imagen,0,0);
+    fondo.cargaOk = true;
+    dibujar();
 });
 vaca.imagen.addEventListener("load",function(){
-     lapiz.drawImage(vaca.imagen,150,100);
+     vaca.cargaOk = true;
+    dibujar();
 });
 cerdo.imagen.addEventListener("load",function(){
-     lapiz.drawImage(cerdo.imagen,200,200);
+     cerdo.cargaOk = true;
+    dibujar();
+});
+cuchillo.imagen.addEventListener("load",function(){
+    cuchillo.cargaOk = true;
+    dibujar();
 });
 function dibujar(){
+    if (fondo.cargaOk== true) {
     lapiz.drawImage(fondo.imagen,0,0);
+    }
+    if (vaca.cargaOk== true) {
     lapiz.drawImage(vaca.imagen,150,100);
+    }
+    if (cerdo.cargaOk== true) {
     lapiz.drawImage(cerdo.imagen,200,200);
+    }
+    if(cuchillo.cargaOk== true) {
+    lapiz.drawImage(cuchillo.imagen,250,250);
+    }
 }
 
